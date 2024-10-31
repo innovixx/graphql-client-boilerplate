2d9efafa3c1807fd63c2895f5ba07a81a8d0de17
Aman Bhogal <aman.bhogal@innovixx.co.uk>
2024-10-31 11:11:15 +0000

    feat: add strong type generation for scss modules

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,6 +1,6 @@
 // / <reference types="vite/client" />
 
 declare module '*.module.scss' {
-  const classes: { [key: string]: string };
-  export default classes;
+  const styles: { [className: string]: string };
+  export default styles;
 }
