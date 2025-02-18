106d0c7eee948795f4384e94be59ce5b0043a0c6
Aman Bhogal <aman.bhogal@innovixx.co.uk>
2025-02-18 13:22:14 +0000

    chore: clean up eslint config and include eslint.config.mjs in tsconfig

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,56 +1,17 @@
-<<<<<<< HEAD
-/** @typedef {import('eslint').Linter.Config} Config */
-
-=======
->>>>>>> cc44518 (chore: update eslint config to v3.0.5)
 import baseConfig from '@innovixx/eslint-config/config/configs/base/index.mjs';
 import reactConfig from '@innovixx/eslint-config/config/configs/react/index.mjs';
 import typescriptConfig from '@innovixx/eslint-config/config/configs/typescript/index.mjs';
 
-<<<<<<< HEAD
-export const defaultESLintIgnores = [
-  '**/.*',
-  '**/.git',
-  '**/README.md',
-  '**/dist/',
-  '**/build/',
-  '**/node_modules/',
-  '**/temp/',
-];
-
-/** @type {Config[]} */
-export const rootEslintConfig = [
-=======
 export default [
->>>>>>> cc44518 (chore: update eslint config to v3.0.5)
   baseConfig,
   reactConfig,
   typescriptConfig,
   {
     ignores: [
-<<<<<<< HEAD
-      ...defaultESLintIgnores,
-      'src/graphql/generated/schema.ts',
-      'vite.config.mjs',
-=======
-      'src/graphql/generated/*',
->>>>>>> cc44518 (chore: update eslint config to v3.0.5)
+      'src/graphql/generated',
     ],
   },
   {
     files: ['**/*.{js,jsx,ts,tsx}'],
   },
-<<<<<<< HEAD
-  {
-    rules: {
-      'react/require-default-props': 0,
-    },
-  },
 ];
-
-export default [
-  ...rootEslintConfig,
-];
-=======
-];
->>>>>>> cc44518 (chore: update eslint config to v3.0.5)
